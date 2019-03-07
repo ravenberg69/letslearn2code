f9d1f0238f7978d8f01eadae2120cfb414fc41ac
ravenberg69 <flo20deg@gmx.de>
2019-03-07 21:34:02 +0100

    Updates

---
+++ precache-manifest.44b1ca56d22b9cc3606fd5895298ebf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f07716ee0e579e066b",
+    "url": "/letslearn2code/static/css/main.92e02293.chunk.css"
+  },
+  {
+    "revision": "e9f07716ee0e579e066b",
+    "url": "/letslearn2code/static/js/main.4076f8d8.chunk.js"
+  },
+  {
+    "revision": "a15c7d9f1dff5fec8db3",
+    "url": "/letslearn2code/static/js/runtime~main.a9274016.js"
+  },
+  {
+    "revision": "3dfe7c05e380988cab49",
+    "url": "/letslearn2code/static/js/2.c5455b9d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/letslearn2code/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2540093e41b4a112c4cf618112f18c68",
+    "url": "/letslearn2code/index.html"
+  }
+];
